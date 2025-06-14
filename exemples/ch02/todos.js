2ab27a8bdb694d12cf27d62a6675fc37a7c2f54f
Gladimir Jacobsen <gladimirjacobsen@gmail.com>
2025-06-14 00:15:48 -0400

    Initial state

---
+++ exemples/ch02/todos.js
@@ -0,0 +1,6 @@
+const todos = ['Walk the dog', 'Wather the plant', 'Sand the chairs'];
+
+const addTodoinput = document.getElementById('todo-input');
+const addTodoButton = document.getElementById('add-btn');
+
+const todoLists = document.getElementById('todos-list');
