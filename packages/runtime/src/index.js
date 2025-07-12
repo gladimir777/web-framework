c38ada649892dc200405d66efe34c3a5117a848a
infogtechht <gladimirjacobsen@gmail.com>
2025-07-11 23:45:03 -0400

    implement virtual dom function

--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -1,1 +1,9 @@
+import { h } from './h';
+
 console.log('This will soon be a frontend frame work!');
+
+h('form', { class: 'login-form', action: 'login' }, [
+  h('input', { type: 'text', name: 'user' }),
+  h('input', { type: 'password', name: 'pass' }),
+  h('button', { on: { click: login } }, ['Log in'])
+]);
